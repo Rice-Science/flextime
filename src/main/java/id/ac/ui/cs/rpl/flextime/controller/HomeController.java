fb90bc5bb7e1eb5f2f096e47758f6ae80ca3028c
cyrilladrian <cyrilladrian.wicaksono@gmail.com>
2024-05-17 20:52:11 +0700

    Add skeleton for functions FCPS

--- src/main/java/id/ac/ui/cs/rpl/flextime/controller/HomeController.java
+++ src/main/java/id/ac/ui/cs/rpl/flextime/controller/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 import java.util.Optional;
@@ -47,6 +48,21 @@ public class HomeController {
         model.addAttribute("classSchedules", classSchedules);
 
         return "home";
+    }
+
+    @PostMapping("")
+    public String addSessionToFitnessPlan(Model model){
+        return "";
+    }
+
+    @PostMapping("")
+    public String deleteSessionFromFitnessPlan(){
+        return "";
+    }
+
+    @PostMapping
+    public String editSessionFromFitnessPlan(){
+        return "";
     }
 
 
