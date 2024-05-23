57ddb8b345c0fbfae2e233dc5697f388cb6a9e3d
cyrilladrian <cyrilladrian.wicaksono@gmail.com>
2024-05-23 21:25:22 +0700

    FEAT: create model for the activity plan model

--- src/main/java/id/ac/ui/cs/rpl/flextime/model/ActivityPlan.java
+++ src/main/java/id/ac/ui/cs/rpl/flextime/model/ActivityPlan.java
@@ -1,4 +1,33 @@
 package id.ac.ui.cs.rpl.flextime.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Table(name="tbl_activity")
 public class ActivityPlan {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID activityId;
+
+    @ManyToOne
+    @JoinColumn(name="class_schedule_id")
+    private ClassSchedules classSchedules;
+
+    @ManyToOne
+    @JoinColumn(name="assignment_schedule_id ")
+    private AssignmentSchedules assignmentSchedules;
+
+    @ElementCollection
+    private Map<Date, UUID> sessionSchedules;
+
 }
